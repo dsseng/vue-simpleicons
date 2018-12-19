1c43a167462f9ce404bc5e94139d399ed7e17bdc
sh7dm <d3dx12.xx@gmail.com>
2018-12-19 16:52:56 +0300

    test: fix tests

--- test/VueSimpleIcon.test.ts
+++ test/VueSimpleIcon.test.ts
@@ -2,7 +2,13 @@ import { createLocalVue, shallowMount } from "@vue/test-utils";
 import VueSimpleIcon from "../src/VueSimpleIcon";
 
 describe('VueSimpleIcon', () => {
-  it('should exists', () => {
-    const vm = shallowMount(VueSimpleIcon);
+  it('should render icon', () => {
+    const wrapper = shallowMount(VueSimpleIcon, {
+      propsData: {
+        name: 'Google'
+      }
+    });
+
+    expect(wrapper.html()).toMatchSnapshot();
   });
 });

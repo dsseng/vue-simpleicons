2488926eb7b0f70d7149e8629979c5362e74f1ff
gabrieledarrigo <darrigo.g@gmail.com>
2018-12-19 09:21:07 +0100

    Add Jest and relative dependencies in order to run tests

---
+++ test/VueSimpleIcon.test.ts
@@ -0,0 +1,8 @@
+import { createLocalVue, shallowMount } from "@vue/test-utils";
+import VueSimpleIcon from "../src/VueSimpleIcon";
+
+describe('VueSimpleIcon', () => {
+  it('should exists', () => {
+    const vm = shallowMount(VueSimpleIcon);
+  });
+});

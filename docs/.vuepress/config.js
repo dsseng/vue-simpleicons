5be50485b222def6dc77701fb38474e05f236b01
sh7dm <d3dx12.xx@gmail.com>
2018-12-20 16:54:04 +0300

    docs: add "contribute" and "help us improve this page links"

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,12 @@ module.exports = {
     sidebar: [
       '/',
       '/API'
-    ]
+    ],
+    repo: 'sh7dm/vue-simple-icons',
+    repoLabel: 'Contribute!',
+    docsDir: 'docs',
+    docsBranch: 'vuepress', // TODO: change to `master` before merging
+    editLinks: true,
+    editLinkText: 'Help us improve this page!'
   }
 }

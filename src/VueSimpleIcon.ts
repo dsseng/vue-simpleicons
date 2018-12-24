86a42ec0d5a45c673a8dd1c85bdea6013bc4ddbe
sh7dm <d3dx12.xx@gmail.com>
2018-12-24 19:07:53 +0300

    feat: add color validation
    fix #15

--- src/VueSimpleIcon.ts
+++ src/VueSimpleIcon.ts
@@ -53,7 +53,23 @@ export default class VueSimpleIcon extends Vue {
   })
   name!: string;
 
-  @Prop(String)
+  @Prop({
+    type: String,
+    validator: color => {
+      if (color === "") return false;
+      if (color === "inherit") return false;
+      if (color === "transparent") return false;
+
+      const el = document.createElement("div");
+
+      el.style.color = "rgb(0, 0, 0)";
+      el.style.color = color;
+      if (el.style.color !== "rgb(0, 0, 0)") return true;
+      el.style.color = "rgb(255, 255, 255)";
+      el.style.color = color;
+      return el.style.color !== "rgb(255, 255, 255)";
+    }
+  })
   color!: string;
 
   @Prop(String)

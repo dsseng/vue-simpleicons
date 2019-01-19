69610b532a207c5b31b2a62b51a7c214a1261ca6
Eric Cornelissen <ericornelissen@gmail.com>
2019-01-20 00:18:02 +0200

    refactor: improve renderError function

--- src/VueSimpleIcon.ts
+++ src/VueSimpleIcon.ts
@@ -97,43 +97,9 @@ const renderError = (
         height: iconSize,
         viewBox: "0 0 24 24",
         xmlns: "http://www.w3.org/2000/svg"
+      },
+      domProps: {
+        innerHTML: `<title>${title}</title><circle cx=\"12\" cy=\"12\" r=\"12\" fill=\"red\"></circle><line x1=\"12\" y1=\"20\" x2=\"12\" y2=\"20\" stroke=\"#fff\" fill=\"none\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-miterlimit=\"10\"></line><line x1=\"12\" y1=\"5\" x2=\"12\" y2=\"15\" stroke=\"#fff\" fill=\"none\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-miterlimit=\"10\"></line>`
       }
-    },
-    [
-      h("title", {}, [title]),
-      h("circle", {
-        attrs: {
-          cx: 12,
-          cy: 12,
-          r: 12,
-          fill: "red"
-        }
-      }),
-      h("line", {
-        attrs: {
-          x1: 12,
-          y1: 20,
-          x2: 12,
-          y2: 20,
-          stroke: "#fff",
-          fill: "none",
-          "stroke-width": 2,
-          "stroke-linecap": "round",
-          "stroke-miterlimit": 10
-        }
-      }),
-      h("line", {
-        attrs: {
-          x1: 12,
-          y1: 5,
-          x2: 12,
-          y2: 15,
-          stroke: "#fff",
-          fill: "none",
-          "stroke-width": 2,
-          "stroke-linecap": "round",
-          "stroke-miterlimit": 10
-        }
-      })
-    ]
+    }
   );

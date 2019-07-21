fa8f19ffa2837852734535a42866ec774eeb4fd9
Eric Cornelissen <ericornelissen@gmail.com>
2019-07-21 12:10:28 +0100

    build: minify umd package

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,31 @@
 import typescript from "rollup-plugin-typescript2";
+import { uglify } from "rollup-plugin-uglify";
 
-export default {
-  input: "./src/index.ts",
+export default [
+  {
+    input: "./src/index.ts",
 
-  plugins: [typescript()],
+    plugins: [typescript(), uglify()],
 
-  output: [
-    {
-      file: "dist/vue-simpleicons.umd.js",
-      format: "umd",
-      name: "vueSimpleIcons"
-    },
-    {
-      file: "dist/vue-simpleicons.esm.js",
-      format: "esm",
-      name: "vueSimpleIcons"
-    }
-  ]
-};
+    output: [
+      {
+        file: "dist/vue-simpleicons.umd.js",
+        format: "umd",
+        name: "vueSimpleIcons"
+      }
+    ]
+  },
+  {
+    input: "./src/index.ts",
+
+    plugins: [typescript()],
+
+    output: [
+      {
+        file: "dist/vue-simpleicons.esm.js",
+        format: "esm",
+        name: "vueSimpleIcons"
+      }
+    ]
+  }
+];

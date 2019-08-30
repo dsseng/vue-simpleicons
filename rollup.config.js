a43ca44411f973188d918018fa09dc2ef7d168c1
Eric Cornelissen <ericornelissen@gmail.com>
2019-08-30 12:47:48 +0300

    Fix build warnings (#100)
    * fix(build): use resolve plugin to resolve dependencies
    
    As per the current warnings when runnin `$ yarn build`:
    https://rollupjs.org/guide/en/#warning-treating-module-as-external-dependency
    
    * chore: change rollup-plugin-node-resolve to devDependency

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
 import typescript from "rollup-plugin-typescript2";
+import resolve from "rollup-plugin-node-resolve";
 
 export default {
   input: "./src/index.ts",
 
-  plugins: [typescript()],
+  plugins: [typescript(), resolve()],
 
   output: [
     {
